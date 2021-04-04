9d39656eefaf70784efbb065fb675b892a38b4f8
Thorge Hemken <thorge@flo.rip>
2021-04-04 13:53:26 +0200

    reverted to bootstrap, because i suck at css...

--- app/.svelte/dev/generated/manifest.js
+++ app/.svelte/dev/generated/manifest.js
@@ -1,7 +1,11 @@
-import * as layout from "../components/layout.svelte";
+import * as layout from "../../../src/routes/$layout.svelte";
 
 const components = [
-	() => import("../../../src/routes/index.svelte")
+	() => import("../../../src/routes/index.svelte"),
+	() => import("../../../src/routes/about.svelte"),
+	() => import("../../../src/routes/game/index.svelte"),
+	() => import("../../../src/routes/game/create.svelte"),
+	() => import("../../../src/routes/game/join.svelte")
 ];
 
 const d = decodeURIComponent;
@@ -9,7 +13,19 @@ const empty = () => ({});
 
 export const routes = [
 	// src/routes/index.svelte
-[/^\/$/, [components[0]]]
+[/^\/$/, [components[0]]],
+
+// src/routes/about.svelte
+[/^\/about\/?$/, [components[1]]],
+
+// src/routes/game/index.svelte
+[/^\/game\/?$/, [components[2]]],
+
+// src/routes/game/create.svelte
+[/^\/game\/create\/?$/, [components[3]]],
+
+// src/routes/game/join.svelte
+[/^\/game\/join\/?$/, [components[4]]]
 ];
 
 export { layout };
---
+++ app/src/routes/$layout.svelte
@@ -0,0 +1,58 @@
+<script>
+  import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    ButtonGroup,
+    Button,
+  } from "sveltestrap";
+
+  let isOpen = false;
+  const toggle = () => (isOpen = !isOpen);
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+  />
+
+  <title>Guess</title>
+</svelte:head>
+
+<header>
+  <Navbar color="dark" dark>
+    <NavbarBrand href="/">GUESS</NavbarBrand>
+
+    <ButtonGroup>
+      <a href="/game/join">
+        <Button color="primary">Join Game</Button>
+      </a>
+      <a href="/game/create">
+        <Button color="success">Create</Button>
+      </a>
+    </ButtonGroup>
+
+    <NavbarToggler on:click={toggle} className="mr-2" />
+    <Collapse {isOpen} navbar>
+      <Nav navbar>
+        <NavItem>
+          <NavLink href="/about">About</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://github.com/thorge2/guess" target="_blank"
+            >GitHub</NavLink
+          >
+        </NavItem>
+      </Nav>
+    </Collapse>
+  </Navbar>
+</header>
+
+<main>
+  <slot />
+</main>
